14e6dd26f249b93d92b69a9b8646647ba48307c9
FranMT-S <franciscomadridtoledo@gmail.com>
2024-04-12 00:43:33 -0600

    first commit

---
+++ Syntax analyzer/main.py
@@ -0,0 +1,14 @@
+from src import *
+import re
+
+
+
+if __name__ == "__main__":
+
+
+    analizer = SyntaxAnalizer();
+    analizer.token_ignore_newline(3)
+
+           
+    
+   
---
+++ Syntax analyzer/src/core/core.py
@@ -0,0 +1,9 @@
+import re
+
+class SyntaxAnalizer:
+    def __init__(self) -> None:
+        pass
+    
+    def token_ignore_newline(self,token:str) -> str:
+        print(str(token) + " test")
+    
